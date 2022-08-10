085879745a031a1358ed49742e25e80463b37f91
Z <Email@Email.com>
2022-08-10 11:46:55 -0500

    update chess

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ const config = {
 	plugins: [sveltekit()],
 	ssr: {
 		noExternal: [
-			'@fortawesome/free-regular-svg-icons/', '@fortawesome/free-solid-svg-icons']}
+			'@fortawesome/free-regular-svg-icons', '@fortawesome/free-solid-svg-icons']}
 };
 
 export default config;
